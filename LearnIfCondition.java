478be2d90ca045b623ded5a01d1b9356485817a3
vikram k <vikramkarthick3505vr46@gmail.com>
2025-12-05 10:07:39 +0530

    LearnIfCondition

---
+++ LearnIfCondition.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class LearnIfCondition {
+	
+	public static void main(String[] args) {
+		int number=0;
+		//  0>0
+		if(number>0) {
+			System.out.println("the number is positive");
+		}
+		 // 0<0
+		else if (number<0) {
+			System.out.println("the number is negative");
+			
+		}
+		else {
+			System.out.println("The Number is zero");
+		}
+	}
+
+}
